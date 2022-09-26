b77cb17908b08a62b23372c00a9f7b3493224a2d
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-26 21:30:13 +0100

    added contact and social media objects.

--- schemas/settings.js
+++ schemas/settings.js
@@ -13,5 +13,68 @@ export default {
             type: 'Image',
             title: 'Website Logo',
         },
+        {
+            name: 'contact',
+            type: 'object',
+            title: 'Contact',
+            fields: [
+                {
+                    name: 'adress',
+                    type: 'string',
+                    title: 'Adress',
+                },
+                {
+                    name: 'phone',
+                    type: 'string',
+                    title: 'Phone',
+                },
+                {
+                    name: 'mobile',
+                    type: 'string',
+                    title: 'Mobile',
+                },
+                {
+                    name: 'fax',
+                    type: 'string',
+                    title: 'Fax',
+                },
+                {
+                    name: 'email',
+                    type: 'string',
+                    title: 'Email',
+                },
+            ],
+        },
+        {
+            name: 'media',
+            type: 'object',
+            title: 'Social media',
+            fields: [
+                {
+                    name: 'facebook',
+                    type: 'string',
+                    title: 'Facebook',
+                    description: 'Add facebook link',
+                },
+                {
+                    name: 'instagram',
+                    type: 'string',
+                    title: 'Instagram',
+                    description: 'Add Instagram link',
+                },
+                {
+                    name: 'whatsapp',
+                    type: 'string',
+                    title: 'Whatsapp',
+                    description: 'Add whatsapp link',
+                },
+                {
+                    name: 'tiktok',
+                    type: 'string',
+                    title: 'Tiktok',
+                    description: 'Add tiktok link',
+                },
+            ],
+        },
     ],
 }
