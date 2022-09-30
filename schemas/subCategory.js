4711c689d4d34a11973fd440c205e91734a4513d
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-30 17:07:02 +0100

    added subCategory.

---
+++ schemas/subCategory.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'subCategory',
+    title: 'Sub category',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Titre',
+        },
+        {
+            name: 'image',
+            type: 'Image',
+            title: 'Image',
+        },
+    ],
+}
