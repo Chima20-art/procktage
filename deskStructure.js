6a1722ebf51565473d19272f5bd94aff0eba00fb
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-03 17:37:30 +0100

    added pages to deskStructure.

--- deskStructure.js
+++ deskStructure.js
@@ -1,10 +1,12 @@
 import S from '@sanity/desk-tool/structure-builder'
+import { FcHome, FcSettings, FcFilingCabinet } from 'react-icons/fc'
 
 export default () =>
     S.list()
         .title('Content')
         .items([
             S.listItem()
+                .icon(FcSettings)
                 .title('Site settings')
                 .child(
                     S.editor()
@@ -13,9 +15,28 @@ export default () =>
                         .title('Site settings')
                 ),
             // Add a visual divider (optional)
+            S.listItem()
+                .title('Pages')
+                .icon(FcFilingCabinet)
+                .child(
+                    S.list()
+                        .title('Pages')
+                        .items([
+                            S.listItem()
+                                .icon(FcHome)
+                                .title('Home Page')
+                                .child(
+                                    S.editor()
+                                        .schemaType('homePage')
+                                        .documentId('homePage')
+                                        .title('home Page')
+                                ),
+                        ])
+                ),
             S.divider(),
             // List out the rest of the document types, but filter out the config type
             ...S.documentTypeListItems().filter(
-                (listItem) => !['settings'].includes(listItem.getId())
+                (listItem) =>
+                    !['settings', 'homePage'].includes(listItem.getId())
             ),
         ])
