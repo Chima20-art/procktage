8c3f5afa10b46ccdfc80680caa11bf7e18066451
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-05 20:11:05 +0100

    added ReferencedByView.

--- deskStructure.js
+++ deskStructure.js
@@ -1,5 +1,13 @@
 import S from '@sanity/desk-tool/structure-builder'
 import { FcHome, FcSettings, FcFilingCabinet } from 'react-icons/fc'
+import { ReferencedByView } from 'part:@indent-oss/sanityio-referenced-by'
+
+export const getDefaultDocumentNode = () => {
+    return S.document().views([
+        S.view.form(),
+        S.view.component(ReferencedByView).title('Referenced by'),
+    ])
+}
 
 export default () =>
     S.list()
