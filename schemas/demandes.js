5825d45be10ae7a987ba712c5c0ba98b2c1e7354
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-25 00:19:40 +0100

    added products to demandes.

--- schemas/demandes.js
+++ schemas/demandes.js
@@ -4,6 +4,20 @@ export default {
     title: 'Demandes',
     fields: [
         {
+            name: 'status',
+            title: 'Statut',
+            type: 'string',
+            options: {
+                list: [
+                    { title: 'New', value: 'New' },
+                    { title: 'Confirmed', value: 'Confirmed' },
+                    { title: 'Shiped', value: 'Shiped' },
+                    { title: 'Delivered', value: 'Delivered' },
+                    { title: 'Cancled', value: 'Canceled' },
+                ],
+            },
+        },
+        {
             name: 'entrepriseName',
             type: 'string',
             title: "Nom de l'entreprise",
@@ -32,6 +46,60 @@ export default {
             type: 'string',
             title: 'Message',
             readOnly: true,
+        },
+        {
+            name: 'products',
+            type: 'array',
+            title: 'produits',
+            of: [
+                {
+                    name: 'order',
+                    title: 'Order',
+                    type: 'object',
+                    fields: [
+                        {
+                            name: 'product',
+                            type: 'reference',
+                            title: 'produit',
+                            to: [
+                                {
+                                    type: 'product',
+                                },
+                            ],
+                        },
+                        {
+                            name: 'variant',
+                            title: 'Variant Refrence',
+                            type: 'string',
+                        },
+                        { name: 'quantite', type: 'string', title: 'Quantite' },
+                    ],
+                    preview: {
+                        select: {
+                            productTitle: 'product.title',
+                            variant: 'variant',
+                            quantite: 'quantite',
+                            prodcutImage: 'product.image',
+                        },
+                        prepare({
+                            productTitle,
+                            variant,
+                            quantite,
+                            prodcutImage,
+                        }) {
+                            return {
+                                title: productTitle,
+                                subtitle:
+                                    'Variant ref : ' +
+                                    variant +
+                                    ' - Quantite : ' +
+                                    quantite,
+                                media: prodcutImage,
+                            }
+                        },
+                    },
+                },
+            ],
         },
     ],
 }
