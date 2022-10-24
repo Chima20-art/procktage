f1a518c5404cad9366e1a012198421bbab1e8c45
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-10-24 16:07:29 +0100

    added readOnly to demandes.js

--- schemas/demandes.js
+++ schemas/demandes.js
@@ -1,32 +1,37 @@
 export default {
     name: 'demandes',
     type: 'document',
-    title: 'demandes',
+    title: 'Demandes',
     fields: [
         {
             name: 'entrepriseName',
             type: 'string',
             title: "Nom de l'entreprise",
+            readOnly: true,
         },
         {
             name: 'nomResponsable',
             type: 'string',
             title: 'Nom du responsable',
+            readOnly: true,
         },
         {
             name: 'email',
             type: 'string',
             title: 'Email',
+            readOnly: true,
         },
         {
             name: 'telephone',
             type: 'string',
             title: 'telephone',
+            readOnly: true,
         },
         {
             name: 'message',
             type: 'string',
             title: 'Message',
+            readOnly: true,
         },
     ],
 }
